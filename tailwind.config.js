472de356a0e4c9900ddb1c413f00d4a63229c016
minthehouse <min.navy.lee@gmail.com>
2023-09-27 19:57:50 -0400

    set screensize in tailwind config in order to set style class based on screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    screens: {
+      xs: { max: "375px" },
+      sm: "376px",
+      md: "768px",
+    },
     extend: {},
   },
   plugins: [],
