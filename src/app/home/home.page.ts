b60c6c66828331bf4bdd436f68273a5921b9955e
minthehouse <min.navy.lee@gmail.com>
2023-09-27 19:35:54 -0400

    add error msg to required field and brief intro in ion-card div on home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {
   FormControl,
   FormGroup,
+  FormGroupDirective,
+  NgForm,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
@@ -9,13 +11,14 @@ import { IonicModule } from '@ionic/angular';
 import { NavigateService } from '../services/navigate.service';
 import { Store } from '@ngxs/store';
 import { SetNumberOfPlayers } from 'state/config.action';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, ReactiveFormsModule],
+  imports: [IonicModule, ReactiveFormsModule, CommonModule],
 })
 export class HomePage {
   public form = new FormGroup({
@@ -23,9 +26,7 @@ export class HomePage {
   });
   constructor(public navigateService: NavigateService, private store: Store) {}
 
-  submit(form: FormGroup) {
-    console.log(form);
-
+  submit(form: FormGroupDirective) {
     if (form.status === 'VALID') {
       this.store.dispatch(new SetNumberOfPlayers(form.value.number_of_players));
       this.navigateService.navigate('/play');
